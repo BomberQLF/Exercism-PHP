aaf7d4f04221cdf4ec00f0cb41d2fca0e155b1fc
BomberQLF <tmurphyparis@gmail.com>
2024-10-25 11:11:10 +0200

    HS - (String Manipulation)

---
+++ highscool-sweethearts/index.php
@@ -0,0 +1,43 @@
+<?php
+
+class HighSchoolSweetheart
+{
+    public function firstLetter(string $name): string
+    {
+        $trimmed = trim($name);
+        return $trimmed[0];
+    }
+
+    public function initial(string $name): string
+    {
+        return strtoupper($this->firstLetter($name)) . ".";
+    }
+
+    public function initials(string $name): string
+    {
+        $names = explode(' ', $name);
+        return $this->initial($names[0]) . ' ' . $this->initial($names[1]);
+    }
+    public function pair(string $sweetheart_a, string $sweetheart_b): string
+    {
+        $a = $this->initials($sweetheart_a);
+        $b = $this->initials($sweetheart_b);
+        return <<<HEART
+        ******       ******
+      **      **   **      **
+    **         ** **         **
+   **            *            **
+   **                         **
+   **     $a  +  $b     **
+    **                       **
+      **                   **
+        **               **
+          **           **
+            **       **
+              **   **
+                ***
+                 *
+   HEART;
+    }
+}
+
