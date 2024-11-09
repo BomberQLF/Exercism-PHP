b0d26b0e3ecd9433a9941c42ebeaafc24dd2e295
BomberQLF <tmurphyparis@gmail.com>
2024-11-09 11:51:17 +0100

    Language List (Array)

---
+++ language-list/index.php
@@ -0,0 +1,21 @@
+<?php
+    function language_list(...$arg)
+    {
+        return $arg;
+    }
+    
+    function add_to_language_list($language_list, $lang) {
+        $language_list[] = $lang;
+        return $language_list;
+    }
+    
+    function prune_language_list($language_list) {
+        array_shift($language_list);
+        return $language_list;
+    }
+    function current_language($language_list) {
+        return $language_list[0];
+    }
+    function language_list_length($language_list) {
+        return count($language_list);
+    }
